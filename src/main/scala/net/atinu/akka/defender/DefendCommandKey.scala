28470a637fd5f5d949d8e784b9cfe9425232d3eb
Tobias Neef <tobnee@gmail.com>
2015-12-21 23:18:46 +0100

    Inline command key instances, fixes #2

--- src/main/scala/net/atinu/akka/defender/DefendCommandKey.scala
+++ src/main/scala/net/atinu/akka/defender/DefendCommandKey.scala
@@ -1,16 +1,25 @@
 package net.atinu.akka.defender
 
+import java.util.concurrent.ConcurrentHashMap
+
 object DefendCommandKey {
 
-  def apply(_name: String) = new DefaultDefendCommandKey {
-    override def name: String = _name
-  }
+  private val intern = new ConcurrentHashMap[String, DefendCommandKey]()
+
+  def apply(_name: String) =
+    if (intern.containsKey(_name)) intern.get(_name)
+    else {
+      val key = DefaultDefendCommandKey(_name)
+      intern.put(_name, key)
+      key
+    }
 }
 
 trait DefendCommandKey {
   def name: String
 }
 
-trait DefaultDefendCommandKey extends DefendCommandKey {
+case class DefaultDefendCommandKey(name: String) extends DefendCommandKey {
+
   override def toString = name
 }
