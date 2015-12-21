3f0cca24673554167718ec433d7da3cad81271ab
Tobias Neef <tobnee@gmail.com>
2015-12-21 22:34:07 +0100

    Make command parameters covariant

--- src/main/scala/net/atinu/akka/defender/AsyncDefendExecution.scala
+++ src/main/scala/net/atinu/akka/defender/AsyncDefendExecution.scala
@@ -7,21 +7,26 @@ trait NamedCommand {
   def cmdKey: DefendCommandKey
 }
 
-trait DefendExecution[R, E] extends NamedCommand {
+trait DefendExecution[+R, +E] extends NamedCommand {
 
   def execute: E
 }
 
-trait AsyncDefendExecution[R] extends NamedCommand with DefendExecution[R, Future[R]]
+trait AsyncDefendExecution[+R] extends NamedCommand with DefendExecution[R, Future[R]]
 
-trait SyncDefendExecution[R] extends NamedCommand with DefendExecution[R, R]
+trait SyncDefendExecution[+R] extends NamedCommand with DefendExecution[R, R]
 
-trait StaticFallback[R] { self: DefendExecution[R, _] =>
+trait StaticFallback[+R] { self: DefendExecution[R, _] =>
 
   def fallback: R
 }
 
-trait CmdFallback[R] { self: DefendExecution[R, _] =>
+trait CmdFallback[+R] { self: DefendExecution[R, _] =>
 
   def fallback: DefendExecution[R, _]
+}
+
+trait SuccessCategorization[R] { self: DefendExecution[R, _] =>
+
+  def categorize: PartialFunction[R, SuccessCategory]
 }
